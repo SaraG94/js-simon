accd3cc159cec9b1009427414f3c2d53d4b70c63
SaraG94 <sara.gherbassi.94@gmail.com>
2023-02-13 18:20:16 +0100

    FEAT timer

--- js/app.js
+++ js/app.js
@@ -1,23 +1,48 @@
 console.log('ciao');
 
 //mostra all'utente numeri random
-alert ('Memorizza questi numeri'+ ' ' + numeriRandom());
+alert ('Memorizza questi numeri'+ ' ' + randomNumbs());
+
+// timer di 30 secondi
+
+let counter = 30
+
+let clock = setInterval(onInterval, 1000)
 
 //numero random da memorizzare
-function numeriRandom(){
-   let numeri = [];
+function randomNumbs(){
+   let numbers = [];
 
    for(let i = 0; i < 5; i++){
         let numbMemo = Math.floor(Math.random() * 100) + 1;
         console.log(numbMemo);
 
-        numeri.push(numbMemo);
+        numbers.push(numbMemo);
     }
 
-    console.log(numeri);
+    console.log(numbers);
 
-    return numeri;
+    return numbers;
 }
+
+function onInterval() {
+    counter--
+
+    console.log(counter)
+
+    if (counter === 0) {
+        //interrompere
+        clearInterval(clock)
+        
+    }
+}
+
+
+
+
+//l’utente inserisce, uno alla volta, i numeri, tramite il prompt()
+//Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali numeri sono stati individuati tramite console.log
+
 
 
 
