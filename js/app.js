a37e52faa682f3cdfb50aaf6dc29d7c290c0d14f
SaraG94 <sara.gherbassi.94@gmail.com>
2023-02-13 17:53:50 +0100

    FEAT numeri random

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
 console.log('ciao');
+
+//mostra all'utente numeri random
+alert ('Memorizza questi numeri'+ ' ' + numeriRandom());
+
+//numero random da memorizzare
+function numeriRandom(){
+   let numeri = [];
+
+   for(let i = 0; i < 5; i++){
+        let numbMemo = Math.floor(Math.random() * 100) + 1;
+        console.log(numbMemo);
+
+        numeri.push(numbMemo);
+    }
+
+    console.log(numeri);
+
+    return numeri;
+}
+
+
+
